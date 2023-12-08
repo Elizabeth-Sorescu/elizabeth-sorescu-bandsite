37626f1928f0b94b7a2cf741ed49e68c06556846
Elizabeth Sorescu <elizabeth.sorescu@gmail.com>
2023-12-07 22:29:11 -0800

    add mobile response for comments section

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,2 +1,11 @@
 // const title = document.getElementsByClassName("nav__header__link");
 // console.log(title);
+
+// comments={
+//     comment1={
+//         name: "Connor Walton",
+//         timeStamp: "02/17/2021",
+//         comment: "This is art. This is inexplicable magic expressed in the purest way,
+//     everything that makes up this majestic work deserves reverence. Let
+//     us appreciate this for what it is and what it contains.", }
+// }
